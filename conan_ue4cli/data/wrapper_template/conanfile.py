00a8a8c3ada7cbc2584737f1b268da555a284c64
metalclaw <zeratul11@hotmail.com>
2025-01-10 14:31:28 +0100

    testing

--- conan_ue4cli/data/wrapper_template/conanfile.py
+++ conan_ue4cli/data/wrapper_template/conanfile.py
@@ -27,7 +27,8 @@ class ${LIBNAME}Conan(ConanFile):
         return os.path.join(self.package_folder, "flags.json")
     
     def package(self):
-        print(self.runenv_info.vars(self).get('PYTHONPATH'))
+        print(self.runenv_info.vars(self))
+        print(self.buildenv_info.vars(self))
         # Retrieve the details for the wrapped library from ue4cli
         from ue4lib import UE4Lib
         details = UE4Lib("${LIBNAME}")
