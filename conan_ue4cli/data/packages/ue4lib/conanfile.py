14775f875d6e93ab1ab2f404536ecdd7f7597b76
metalclaw <zeratul11@hotmail.com>
2025-01-10 11:44:07 +0100

    testing

--- conan_ue4cli/data/packages/ue4lib/conanfile.py
+++ conan_ue4cli/data/packages/ue4lib/conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
-from conan.tools.files import copy as conancopy
-import os, copy
+from conan.tools.files import copy
+import os
 
 class UE4LibConan(ConanFile):
     name = "ue4lib"
@@ -20,8 +20,9 @@ class UE4LibConan(ConanFile):
     build_policy = "missing"
     
     def package(self):
-        conancopy(self, "*.py", self.source_folder, self.package_folder)
+        copy(self, "*.py", self.source_folder, self.package_folder)
     
     def package_info(self):
-        self.runenv_info.append_path("PYTHONPATH", self.package_folder)
+        # self.runenv_info.append_path("PYTHONPATH", self.package_folder)
+        os.environ['PYTHONPATH'].append(self.package_folder)
         # print("Python path: " + self.runenv_info.vars(self).get('PYTHONPATH'))
