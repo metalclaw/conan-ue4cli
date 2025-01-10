2f3f4df00a3151eb02920fad34ef6abe2c832641
metalclaw <zeratul11@hotmail.com>
2025-01-10 15:45:58 +0100

    testing

--- conan_ue4cli/data/packages/ue4lib/conanfile.py
+++ conan_ue4cli/data/packages/ue4lib/conanfile.py
@@ -22,7 +22,7 @@ class UE4LibConan(ConanFile):
         copy(self, "*.py", self.source_folder, self.package_folder)
     
     def package_info(self):
-        self.conf_info.define("PYTHONPATH", self.package_folder)
+        self.conf_info.define("pythonpath", self.package_folder)
         self.runenv.append_path("PYTHONPATH", self.package_folder)
         self.buildenv.append_path("PYTHONPATH", self.package_folder)
 
