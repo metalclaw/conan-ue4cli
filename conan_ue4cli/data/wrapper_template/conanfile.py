82788fc1ee42fa24c77723047072f797f4554199
metalclaw <zeratul11@hotmail.com>
2025-01-13 12:52:53 +0100

    testing

--- conan_ue4cli/data/wrapper_template/conanfile.py
+++ conan_ue4cli/data/wrapper_template/conanfile.py
@@ -73,7 +73,7 @@ class ${LIBNAME}Conan(ConanFile):
             "exelinkflags":    details.ldflags(),
             "systemlibs":      systemLibs
         }
-        tools.save(self.flags_filename(), json.dumps(flags))
+        tools.files.save(self.flags_filename(), json.dumps(flags))
         
         # Perform any package-specific post-build logic
         PackageDelegate.post_build(self)
